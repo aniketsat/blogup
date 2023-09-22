12c60ecb97fc3faf1f467ca3aa9bcddc1e6395a4
Aniket Satapathy <aniketsatapathy193@gmail.com>
2023-09-22 21:20:52 +0530

    folder structure updated

--- backend/server.js
+++ backend/server.js
@@ -15,25 +15,16 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/posts", require("./routes/postRoutes"));
 app.use("/api/likes", require("./routes/likeRoutes"));
 app.use("/api/saves", require("./routes/saveRoutes"));
 app.use("/api/follows", require("./routes/followRoutes"));
 app.use("/api/comments", require("./routes/commentRoutes"));
-
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
 
 app.use(errorHandler);
 
